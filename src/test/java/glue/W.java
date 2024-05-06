305007805aafd19e58cb10bde94ad2aa6a09d706
chetan <chetanss79@gmail.com>
2024-05-06 18:10:05 +0100

    As I have updated driver commenting out webdriver.chrome.driver

--- src/test/java/glue/W.java
+++ src/test/java/glue/W.java
@@ -18,12 +18,12 @@ public class W {
     protected WebDriver driver;
 
     private W() {
-        String pathToDriver = System.getProperty("webdriver.chrome.driver");
+       /* String pathToDriver = System.getProperty("webdriver.chrome.driver");
         if (pathToDriver == null || pathToDriver.isEmpty()) {
             throw new RuntimeException("define a path to the chrome driver using system property 'webdriver.chrome.driver'");
         }
         System.setProperty("webdriver.chrome.driver", pathToDriver);
-
+*/
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.manage().window().maximize();
