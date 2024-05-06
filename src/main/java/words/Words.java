73c7b38ea0ba93bf6d97ff1fc467e488501cd841
chetan <chetanss79@gmail.com>
2024-05-06 17:46:09 +0100

    exercise1 completed

--- src/main/java/words/Words.java
+++ src/main/java/words/Words.java
@@ -1,9 +1,20 @@
 package words;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Words {
+    /*
+    sentence.toLowerCase() converts the entire sentence to lowercase to ensure case-insensitivity.
+    .split("\\W+") splits the sentence into words using a regular expression that matches any sequence of non-word characters. This effectively removes punctuation.
+    Arrays.stream(...) creates a stream from the array of words.
+    .distinct() filters the stream to only include unique elements, removing duplicates.
+    .collect(Collectors.toList()) collects the elements of the stream into a List.
+     */
     public static List<String> getUniqueWordsFromSentence(String sentence) {
-        throw new UnsupportedOperationException();
+        return Arrays.stream(sentence.toLowerCase().split("\\W+"))
+                .distinct()
+                .collect(Collectors.toList());
     }
 }
